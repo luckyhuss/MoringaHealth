5bccc6eeae73fad1ba5dd9ba5a0208e4b227b93d
Anwar Buchoo <luckyhuss@msn.com>
2017-12-31 10:13:27 +0400

    add Favico

--- include/header.php
+++ include/header.php
@@ -1,6 +1,11 @@
 <!-- developped by Anwar Buchoo (http://ideaof.me / luckhuss@msn.com) -->
   <meta name="keywords" content="v kanhye health foods moringa miracle plant" />
   <meta name="description" content="" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="theme-color" content="#ffffff">
   <!-- start includes -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
